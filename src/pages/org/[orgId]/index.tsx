9b52801d27aa64f47e96ba46d47060b5ac23dba7
marco debernardi <marco.debernardi.1982@gmail.com>
2023-07-11 17:57:17 +0200

    add console to check organization

--- src/pages/org/[orgId]/index.tsx
+++ src/pages/org/[orgId]/index.tsx
@@ -1,6 +1,7 @@
 import SurveyContent from "@/component/SurveyContent";
 import { OrgContext } from "@/helper/context";
 import { getOrganization } from "@/pages/api/autosurvey";
+import { Organization } from "@/type/type";
 import { useRouter } from "next/router"
 import { useContext, useEffect } from "react";
 
@@ -15,13 +16,13 @@ export default function OrgDetails() {
   useEffect(() => {
     console.log("router.query in useEffect: ", router.query);
     if (orgid) {
-      getOrganization(orgid, setOrganization);
-     
+      const organization =  getOrganization(orgid, setOrganization);
+      console.log("organization: ", organization);
     }
     
   }, [orgid])
 
-  console.log("router.query after useEffect: ", router.query);
+  console.log("router.query after: ", router.query);
   return (
     <div>
       {//orgid && 
