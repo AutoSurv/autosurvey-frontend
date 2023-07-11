209648999788e57aa45ab85cd0a92f1f191f3899
marco debernardi <marco.debernardi.1982@gmail.com>
2023-07-11 18:13:24 +0200

    removed all console.log

--- src/pages/org/[orgId]/index.tsx
+++ src/pages/org/[orgId]/index.tsx
@@ -1,7 +1,6 @@
 import SurveyContent from "@/component/SurveyContent";
 import { OrgContext } from "@/helper/context";
 import { getOrganization } from "@/pages/api/autosurvey";
-import { Organization } from "@/type/type";
 import { useRouter } from "next/router"
 import { useContext, useEffect } from "react";
 
@@ -11,20 +10,13 @@ export default function OrgDetails() {
   const  router  = useRouter();
   const { orgId } = router.query;
   const { setOrganization} =useContext(OrgContext);
-  console.log("router.query.orgId before useEffect: ", router.query.orgId);
-  console.log("orgid before useEffect: ", orgId);
+
   useEffect(() => {
-    console.log("router.query.orgId in useEffect: ", router.query.orgId);
-    console.log("orgid before useEffect: ", orgId);
     if (orgId) {
-      const organization =  getOrganization(orgId as string, setOrganization);
-      console.log("organization: ", organization);
+      getOrganization(orgId as string, setOrganization);
     }
     
   }, [router.query.orgId])
-
-  console.log("router.query.orgId after: ", router.query.orgId);
-  console.log("orgid before useEffect: ", orgId);
 
   return (
     <div>
