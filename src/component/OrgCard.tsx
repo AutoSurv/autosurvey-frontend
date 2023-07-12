7209ea4f2689bfecadbff94116d0198b5d632457
marco debernardi <marco.debernardi.1982@gmail.com>
2023-07-12 14:49:14 +0200

    hide edit/delete org from user

--- src/component/OrgCard.tsx
+++ src/component/OrgCard.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Organization } from '@/type/type';
-import { Dispatch, SetStateAction, useContext, useState } from 'react';
+import { Dispatch, SetStateAction, useContext, useEffect, useState } from 'react';
 import { Button, Form, Input, Label, Modal, Card, Image } from 'semantic-ui-react';
 import { deleOrganization, updateOrganizationName } from '@/pages/api/autosurvey';
 import { OrgContext } from '@/helper/context';
@@ -15,9 +15,14 @@ export default function OrgCard(props: OrgCardProp) {
   const { organization, setOrganizations } = props;
   const { setOrganization } = useContext(OrgContext);
   const [open, setOpen] = useState(false);
+  const [role, setRole] = useState("");
   const [errMessage, setErrMessage] = useState<string>("");
   const orgLowerName: string = organization.orgName.toLowerCase();
 
+
+  useEffect(() => {
+    setRole(localStorage.getItem("role") as string);
+  }, []);
 
   return (
 
@@ -30,41 +35,43 @@ export default function OrgCard(props: OrgCardProp) {
             <Card.Header>{organization.orgName}</Card.Header>
             <Card.Meta>the number of surveys: {organization.surveys.length}</Card.Meta>
           </Card.Content>
-
         </Link>
+        
+        {role !== "ROLE_USER" ?
         <div className="org-card-btn-container">
-        <Modal animation={false}
-          onClose={() => setOpen(false)}
-          onOpen={() => setOpen(true)}
-          open={open}
-          trigger={<Button className="org-modal-btn" basic color="blue"> Edit Name</Button>}>
-          <Modal.Header>Make Your Organization</Modal.Header>
-          <Modal.Content>
-            <Form onSubmit={(e) => {
-              e.preventDefault();
-              updateOrganizationName(organization.orgId, e, setOrganizations, setOrganization, setOpen, setErrMessage);
-            }}>
-              <Form.Field>
-                <Label>Organization Name</Label>
-                <Input placeholder="Name your organization" type="text" name="orgname" />
-              </Form.Field>
-              <Button type="submit" color="blue">Edit</Button>
-              <Button onClick={(e) => {
-                  e.preventDefault();
-                  setOpen(false);
-              }} color="orange"
-              >Cancel</Button>
-            </Form>
-          </Modal.Content>
-        </Modal>
-        <Button className="org-modal-btn" onClick={(e) => {
-          e.preventDefault();
-          deleOrganization(organization.orgId, setOrganizations)
-        }} color="orange" basic
-        >Delete Org.</Button>
+          <Modal animation={false}
+            onClose={() => setOpen(false)}
+            onOpen={() => setOpen(true)}
+            open={open}
+            trigger={<Button className="org-modal-btn" basic color="blue"> Edit Name</Button>}>
+            <Modal.Header>Make Your Organization</Modal.Header>
+            <Modal.Content>
+              <Form onSubmit={(e) => {
+                e.preventDefault();
+                updateOrganizationName(organization.orgId, e, setOrganizations, setOrganization, setOpen, setErrMessage);
+              }}>
+                <Form.Field>
+                  <Label>Organization Name</Label>
+                  <Input placeholder="Name your organization" type="text" name="orgname" />
+                </Form.Field>
+                <Button type="submit" color="blue">Edit</Button>
+                <Button onClick={(e) => {
+                    e.preventDefault();
+                    setOpen(false);
+                }} color="orange"
+                >Cancel</Button>
+              </Form>
+            </Modal.Content>
+          </Modal>
+          <Button className="org-modal-btn" onClick={(e) => {
+            e.preventDefault();
+            deleOrganization(organization.orgId, setOrganizations)
+          }} color="orange" basic
+          >Delete Org.</Button>
         </div>
+        : null
+      }
       </Card>
-
     </div>
   );
 }
