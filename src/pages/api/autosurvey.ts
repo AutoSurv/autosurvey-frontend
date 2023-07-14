5812c7335e21fabca7792967076ba6381716f54f
marco debernardi <marco.debernardi.1982@gmail.com>
2023-07-14 11:38:23 +0200

    fake user for org creation

--- src/pages/api/autosurvey.ts
+++ src/pages/api/autosurvey.ts
@@ -82,7 +82,7 @@ export async function addOrganization(event: React.FormEvent<HTMLFormElement>, s
 
   const reqBody: OrgRequestDto = {
     orgName: event.currentTarget.orgname.value,
-    creator: username
+    creator: "TestUsername"
   };
 
   if (!reqBody.orgName) {
