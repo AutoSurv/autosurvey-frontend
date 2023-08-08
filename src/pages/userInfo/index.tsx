38406179c9b3393c92057cf4cbe5b399fe92086a
marco debernardi <marco.debernardi.1982@gmail.com>
2023-08-08 15:06:05 +0200

    added style and layout to user info page

--- src/pages/userInfo/index.tsx
+++ src/pages/userInfo/index.tsx
@@ -3,10 +3,12 @@ import { useContext, useEffect, useState } from "react";
 import { getUserApi } from "../api/surveyAPI";
 import { User } from "@/type/type";
 import Link from "next/link";
-import { Button } from "semantic-ui-react";
+import { Button, Card, Header, Icon, Label, Menu, Table, TableBody, TableCell, TableRow } from "semantic-ui-react";
+import UserOptions from "@/component/UserOptions";
+import { TableHead } from "@mui/material";
 
 export default function UserInfo() {
-  const { userNameAuth } = useContext(OrgContext);
+  const { userNameAuth, organization } = useContext(OrgContext);
   const [user, setUser] = useState<User>();
    
   const getUserInfo = async () => {
@@ -24,17 +26,82 @@ export default function UserInfo() {
   return (
     
       <main className="org-main">
-        <Link href={"/org"} style={{ textDecoration: 'none' }}>
-          <Button>Back to Org</Button>
-        </Link>
-        
-        <p>
-          <br/>User Info:<br/>
-          <br/><label>Username: {user?.username}</label><br/>
-          <br/><label>Email: {user?.email}</label><br/>
-          <br/><label>Role: {user?.roles}</label><br/>
-          <br/><label>Id: {user?.userId}</label><br/>
-        </p>
+        <div className="home-header-container">
+        <Header
+          className="home-header"
+          as="h1"
+          icon
+          textAlign="center"
+          color="blue"
+        >
+          <Header.Content>
+            <Link href="/org">
+              <Icon name="clipboard" className="home-header-icon" />
+            </Link>
+            <Link className="home-header-autosurvey" href="/org">
+              AutoSurvey
+            </Link>
+          </Header.Content>
+        </Header>
+      </div>
+
+      <Menu size="small" color="blue">
+        <Menu.Item>
+          <Link href={"/org"} style={{ textDecoration: "none" }}>
+            Back to Organizations
+          </Link>
+        </Menu.Item>
+        {
+          organization.orgId.length > 0 ?
+            <>
+              <Menu.Item>
+                <Link href={"/org/" + organization.orgId} style={{ textDecoration: "none" }}>
+                  Back to Surveys
+                </Link>
+              </Menu.Item>
+            </>
+            : null
+        }
+        <Menu.Menu position="right">
+          <Menu.Item>
+            <Link href={"/about"} style={{ textDecoration: 'none' }}>About</Link>
+          </Menu.Item>
+          <UserOptions />
+        </Menu.Menu>
+      </Menu>
+
+      <section className="user-info-section">
+          <Card className="user-card" >
+            <Table className="user-table">
+              <TableBody>
+                <TableRow className="user-table-row">
+                  <TableCell component="th" scope="row" align="left">
+                    Username:
+                  </TableCell>
+                  <TableCell component="th" scope="row" align="right" class="right aligned">
+                    {user?.username}
+                  </TableCell>
+                </TableRow>
+                <TableRow className="user-table-row">
+                  <TableCell component="th" scope="row" align="left">
+                    Email:
+                  </TableCell>
+                  <TableCell component="th" scope="row" align="right" class="right aligned">
+                    {user?.email}
+                  </TableCell>
+                </TableRow>
+                <TableRow className="user-table-row">
+                  <TableCell component="th" scope="row" align="left">
+                    Role:
+                  </TableCell>
+                  <TableCell component="th" scope="row" align="right" class="right aligned">
+                    {user?.roles}
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Card>
+        </section>
       </main>
 
   )
