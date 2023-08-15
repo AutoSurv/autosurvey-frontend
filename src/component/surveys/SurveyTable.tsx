57fe2e9177c950bcec06643947b67c4f0a572750
simon <simonhong85@gmail.com>
2023-08-15 16:30:15 +0200

    working on collapsible

--- src/component/surveys/SurveyTable.tsx
+++ src/component/surveys/SurveyTable.tsx
@@ -11,24 +11,23 @@ import { OrgContext } from '@/helper/context';
 
 type SurveyCardProp = {
     organization: Organization,
-    survey: Survey
+    propSurvey: Survey
 }
 
 export default function SurveyRecord(props: SurveyCardProp) {
-    const { survey, organization } = props;
-    const { setSurvey, setSurveys, setOrganization, setPagination  } = useContext(OrgContext);
+    const { propSurvey, organization } = props;
+    const [actualSurvey, setActualSurevey] = useState(propSurvey);
+    const { survey, setSurvey } = useContext(OrgContext);
     const [open, setOpen] = useState(false);
     const [errMessage, setErrMessage] = useState<string>("");
 
     useEffect(() => {
-        async function fetchSurvey() {
-          const data = await getSurvey(survey.id, setSurvey);
-          console.log("fetch: ", data!.year);
-          console.log("Survey: ", survey.year);
-        }
-           fetchSurvey();
-           console.log("#3:",  survey.year);
-      }, []);
+
+        if (survey.id === propSurvey.id) {        
+            setActualSurevey(survey);
+          }      
+             
+      }, [survey] )
 
     return (
         <>
@@ -43,13 +42,13 @@ export default function SurveyRecord(props: SurveyCardProp) {
                     </IconButton>
                 </TableCell>
                 <TableCell >
-                    <Link className='survey-link' href={"/org/" + organization.orgId + "/" + survey.id}>{survey.id}</Link>
+                    <Link className='survey-link' href={"/org/" + organization.orgId + "/" + survey.id}>{actualSurvey.id}</Link>
                 </TableCell>
                 <TableCell >
-                    <Link className='survey-link' href={"/org/" + organization.orgId + "/" + survey.id}>{survey.country}</Link>
+                    <Link className='survey-link' href={"/org/" + organization.orgId + "/" + survey.id}>{actualSurvey.country}</Link>
                 </TableCell>
                 <TableCell >
-                    <Link className='survey-link' href={"/org/" + organization.orgId + "/" + survey.id}>{survey.year}</Link>
+                    <Link className='survey-link' href={"/org/" + organization.orgId + "/" + survey.id}>{actualSurvey.year}</Link>
                 </TableCell>
                 <TableCell >
                     {localStorage.getItem("username")}
@@ -90,92 +89,93 @@ export default function SurveyRecord(props: SurveyCardProp) {
                             </Table> */}
                             <Form className="surveys-form" onSubmit={(e) => {
                                 e.preventDefault();
-                                updateSurvey(survey.id, e, setSurvey, setOpen, setErrMessage, survey.orgId, setOrganization);
+                                updateSurvey(propSurvey.id, e, setSurvey, setOpen, setErrMessage, propSurvey.orgId);
+                                //getOrganization(organization.orgId, setOrganization);
                             }} >
                                 <Form.Group className="surveys-form-edit" >
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Country</Label>
-                                        <Input placeholder={survey.country} type="text" name="country" pattern="^[a-zA-Z]*$" />
+                                        <Input placeholder={actualSurvey.country} type="text" name="country" pattern="^[a-zA-Z]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Year</Label>
-                                        <Input placeholder={survey.year} type="text" name="year" pattern="^[0-9]*$" />
+                                        <Input placeholder={actualSurvey.year} type="text" name="year" pattern="^[0-9]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Rent</Label>
-                                        <Input placeholder={survey.rent} type="text" name="rent" pattern="^[0-9]*$" />
+                                        <Input placeholder={actualSurvey.rent} type="text" name="rent" pattern="^[0-9]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Utilities</Label>
-                                        <Input placeholder={survey.utilities} type="text" name="utilities" />
+                                        <Input placeholder={actualSurvey.utilities} type="text" name="utilities" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Food</Label>
-                                        <Input placeholder={survey.food} type="text" name="food" pattern="^[0-9]*$" />
+                                        <Input placeholder={actualSurvey.food} type="text" name="food" pattern="^[0-9]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Basic Items</Label>
-                                        <Input placeholder={survey.basicItems} type="text" name="basicItems" pattern="^[0-9]*$" />
+                                        <Input placeholder={actualSurvey.basicItems} type="text" name="basicItems" pattern="^[0-9]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Transportation</Label>
-                                        <Input placeholder={survey.transportation} type="text" name="transportation" />
+                                        <Input placeholder={actualSurvey.transportation} type="text" name="transportation" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Education Total</Label>
-                                        <Input placeholder={survey.educationTotal} type="text" name="educationTotal" />
+                                        <Input placeholder={actualSurvey.educationTotal} type="text" name="educationTotal" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Education Supplies</Label>
-                                        <Input placeholder={survey.educationSupplies} type="text" name="educationSupplies" />
+                                        <Input placeholder={actualSurvey.educationSupplies} type="text" name="educationSupplies" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Education Fee</Label>
-                                        <Input placeholder={survey.educationFee} type="text" name="educationFee" pattern="^[0-9]*$" />
+                                        <Input placeholder={actualSurvey.educationFee} type="text" name="educationFee" pattern="^[0-9]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Education Type</Label>
-                                        <Input placeholder={survey.educationType} type="text" name="educationType" pattern="^[a-zA-Z]*$" />
+                                        <Input placeholder={actualSurvey.educationType} type="text" name="educationType" pattern="^[a-zA-Z]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Accommodation Type</Label>
-                                        <Input placeholder={survey.accommodationType} type="text" name="accommodationType" pattern="^[a-zA-Z]*$" />
+                                        <Input placeholder={actualSurvey.accommodationType} type="text" name="accommodationType" pattern="^[a-zA-Z]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Profession</Label>
-                                        <Input placeholder={survey.profession} type="text" name="profession" pattern="^[a-zA-Z]*$" />
+                                        <Input placeholder={actualSurvey.profession} type="text" name="profession" pattern="^[a-zA-Z]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Location Given</Label>
-                                        <Input placeholder={survey.locationGiven} type="text" name="locationGiven" pattern="^[a-zA-Z]*$" />
+                                        <Input placeholder={actualSurvey.locationGiven} type="text" name="locationGiven" pattern="^[a-zA-Z]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Location Clustered</Label>
-                                        <Input placeholder={survey.locationClustered} type="text" name="locationClustered" pattern="^[a-zA-Z]*$" />
+                                        <Input placeholder={actualSurvey.locationClustered} type="text" name="locationClustered" pattern="^[a-zA-Z]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Number of Residents</Label>
-                                        <Input placeholder={survey.numResidents} type="text" name="numResidents" pattern="^[0-9]*$" />
+                                        <Input placeholder={actualSurvey.numResidents} type="text" name="numResidents" pattern="^[0-9]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Number of Incomes</Label>
-                                        <Input placeholder={survey.numIncomes} type="text" name="numIncomes" pattern="^[0-9]*$" />
+                                        <Input placeholder={actualSurvey.numIncomes} type="text" name="numIncomes" pattern="^[0-9]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Number of Full Incomes</Label>
-                                        <Input placeholder={survey.numFullIncomes} type="text" name="numFullIncomes" pattern="^[0-9]*$" />
+                                        <Input placeholder={actualSurvey.numFullIncomes} type="text" name="numFullIncomes" pattern="^[0-9]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Number of Children</Label>
-                                        <Input placeholder={survey.numChildren} type="text" name="numChildren" pattern="^[0-9]*$" />
+                                        <Input placeholder={actualSurvey.numChildren} type="text" name="numChildren" pattern="^[0-9]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Total Income</Label>
-                                        <Input placeholder={survey.totalIncome} type="text" name="totalIncome" pattern="^[0-9]*$" />
+                                        <Input placeholder={actualSurvey.totalIncome} type="text" name="totalIncome" pattern="^[0-9]*$" />
                                     </Form.Field>
                                     <Form.Field className="surveys-form-edit-field">
                                         <Label>Comments</Label>
-                                        <Input placeholder={survey.comments} type="text" name="comments" pattern="^[a-zA-Z]*$" />
+                                        <Input placeholder={actualSurvey.comments} type="text" name="comments" pattern="^[a-zA-Z]*$" />
                                     </Form.Field>
 
                                 </Form.Group>
