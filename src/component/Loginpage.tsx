3d0c38c3a20871266440e4b980487273f2f6f693
makoalex <macovei.a2@gmail.com>
2023-11-03 09:20:24 +0100

    [feat-3] adding the form and styles to LoadingPage.tsx

--- src/component/Loginpage.tsx
+++ src/component/Loginpage.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useContext, useEffect, useState } from "react";
-import { Message, Modal } from 'semantic-ui-react';
+import { Message, Modal } from "semantic-ui-react";
 import { SignOut, signInJwtTokenHandler } from "@/helper/methods";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
@@ -9,9 +9,16 @@ import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import { OrgContext } from "@/helper/context";
 import { SignUp } from "./SignUp";
+import OfflineButton from "./OfflineButton";
 
 const Login = () => {
-  const { signUpStatus, setSignUpStatus, userNameAuth, setUserNameAuth, setUserDto } = useContext(OrgContext);
+  const {
+    signUpStatus,
+    setSignUpStatus,
+    userNameAuth,
+    setUserNameAuth,
+    setUserDto,
+  } = useContext(OrgContext);
   const [signupSuccessMessage, setSignupSuccessMessage] = useState("");
   const [errorMsg, setErrorMsg] = useState("");
   const [open, setOpen] = useState(false);
@@ -20,68 +27,133 @@ const Login = () => {
     if (typeof window !== "undefined") {
       setSignUpStatus(true);
       setUserNameAuth(localStorage.getItem("username") as string);
-    };
-  }, [])
+    }
+  }, []);
 
   return (
-
     <Container component="main" maxWidth="xs">
+      {signUpStatus === false || !localStorage.getItem("jwt") ? (
+        <>
 
-      {(signUpStatus === false || !localStorage.getItem('jwt')) ?
-        <Box
-          sx={{ marginTop: 8, display: "flex", flexDirection: "column", alignItems: "center", }} >
-          <Box component="form" onSubmit={(e) => {
-            e.preventDefault();
-            signInJwtTokenHandler(e, setErrorMsg, setSignUpStatus, setUserNameAuth, setUserDto);
-          }} noValidate sx={{ mt: 1 }}>
-            <TextField margin="normal" required fullWidth id="username"
-              label="User Name" name="username" autoComplete="username" autoFocus
-            />
-            <TextField margin="normal" required fullWidth name="password" label="Password"
-              type="password" id="password" autoComplete="current-password"
-            />
-            <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
-              Sign In
-            </Button>
-            {(errorMsg.length > 0) ? <Message warning>
-              <p>{errorMsg}</p>
-            </Message> : null}
-            {(signupSuccessMessage.length > 0) ? <Message success>
-              <Message.Header>Your user registration was successful <br />If you wish to be added as manager for the organization, please contact the admins:
-                <br />seongbong.hong@appliedtechnology.se or marco.debernardi@appliedtechnology.se.</Message.Header><br />
-              <Message.Content>You may now log-in with the username you have chosen</Message.Content>
-            </Message> : null}
-          </Box>
+          <form
+            className="form make-offer-form flex flex-col gap-[10px] max-w-[450px] p-[20px] relative mx-auto  "
+            onSubmit={(e) => {
+              e.preventDefault();
+              signInJwtTokenHandler(
+                e,
+                setErrorMsg,
+                setSignUpStatus,
+                setUserNameAuth,
+                setUserDto
+              );
+            }}
+          >
+            <label className="">
+              <input
+                className=" input bg-white placeholder:translate-x-6  align-middle w-full outline-none px-[10px] py-[16px] border border-black/25 opacity-80 rounded-full mt-9 focus:border-primary  "
+                type="text"
+                required
+                id="username"
+                placeholder="Username"
+                name="username"
+              />
+            </label>
+            <label className="">
+              <input
+                className=" bg-white input w-full placeholder:translate-x-6 outline-none px-[10px] py-[16px] border border-black/25 opacity-80 rounded-full  focus:border-primary"
+                name="password"
+                required
+                placeholder="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </label>
+            <button className="bg-primary hover:bg-accent rounded-full text-third h-[50px] cursor-pointer  text-sec font-primary text-lg" type="submit">Sign In</button>
+            <OfflineButton/>
+            {errorMsg.length > 0 ? (
+              <Message warning>
+                <p>{errorMsg}</p>
+              </Message>
+            ) : null}
+            {signupSuccessMessage.length > 0 ? (
+              <Message success>
+                <Message.Header>
+                  Your user registration was successful <br />
+                  If you wish to be added as manager for the organization,
+                  please contact the admins:
+                  <br />
+                  seongbong.hong@appliedtechnology.se or
+                  marco.debernardi@appliedtechnology.se.
+                </Message.Header>
+                <br />
+                <Message.Content>
+                  You may now log-in with the username you have chosen
+                </Message.Content>
+              </Message>
+            ) : null}
+          </form>
           <Grid container>
             <Grid item xs>
               <Modal
                 onClose={() => setOpen(false)}
                 onOpen={() => setOpen(true)}
                 open={open}
-                trigger={<Button className="login-forgotpassword" variant="text" size="small" style={{textTransform: 'none'}}>
-                  Forgot password?
-                </Button>}
+                trigger={
+                  <Button
+                    className="login-forgotpassword"
+                    variant="text"
+                    size="small"
+                    style={{ textTransform: "none" }}
+                  >
+                    Forgot password?
+                  </Button>
+                }
               >
                 <Modal.Header>Forgot password?</Modal.Header>
-                <Modal.Content>Contact: <br /> <i>seonbong.hong@appliedtechnology.se</i> <br /> or <br /> <i>marco.debernardi@appliedtechnology.se</i></Modal.Content>
+                <Modal.Content>
+                  Contact: <br /> <i>seonbong.hong@appliedtechnology.se</i>{" "}
+                  <br /> or <br /> <i>marco.debernardi@appliedtechnology.se</i>
+                </Modal.Content>
                 <Modal.Actions>
-                  <Button onClick={() => {
-                    setOpen(false);
-                  }}>
+                  <Button
+                    onClick={() => {
+                      setOpen(false);
+                    }}
+                  >
                     Close
                   </Button>
                 </Modal.Actions>
               </Modal>
             </Grid>
             <Grid item>
-              <SignUp setErrorMsg={setErrorMsg} setSignupSuccessMessage={setSignupSuccessMessage} />
+              <SignUp
+                setErrorMsg={setErrorMsg}
+                setSignupSuccessMessage={setSignupSuccessMessage}
+              />
             </Grid>
           </Grid>
-        </Box>
-        : <>
-          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}
-            onClick={() => { SignOut(setSignUpStatus) }}> Sign Out User: {userNameAuth}</Button>
-          <Button href="/org" fullWidth>Go to Organization</Button></>}
+    
+        </>
+      ) : (
+        <>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            onClick={() => {
+              SignOut(setSignUpStatus);
+            }}
+          >
+            {" "}
+            Sign Out User: {userNameAuth}
+          </Button>
+          <Button href="/org" fullWidth>
+            Go to Organization
+          </Button>
+        </>
+      )}
     </Container>
   );
 };
