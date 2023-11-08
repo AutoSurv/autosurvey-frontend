3cca8ae690064d7a4c73ff9066d3ab9adc926eb9
makoalex <macovei.a2@gmail.com>
2023-11-08 09:25:26 +0100

    [feat ] adding framer motion to the Modals component

--- src/component/Modals.tsx
+++ src/component/Modals.tsx
@@ -1,29 +1,54 @@
+import { motion, AnimatePresence } from "framer-motion";
 import React, { useState } from "react";
 type Props = {
-  isvisible: boolean,
+  isvisible: boolean;
   // fix this any
-  onClose: any,
-  children?: React.ReactNode
+  onClose: any;
+  children?: React.ReactNode;
 };
 
-export default function Modals({ isvisible, onClose,children }: Props) {
+const backdrop = {
+  visible: { opacity: 1 },
+  hidden: { opacity: 0 },
+};
+const modal = {
+  hidden: { y: "-100vh", opacity: 0 },
+  visible: { y: "80px", opacity: 1, transition: { delay: 0.5 } },
+};
+
+export default function Modals({ isvisible, onClose, children }: Props) {
   if (!isvisible) return null;
-  const handleClose= (e:React.MouseEvent<HTMLDivElement>)=>{
-    if((e.target as HTMLDivElement).id === "wrapper"){
-      onClose()
+  const handleClose = (e: React.MouseEvent<HTMLDivElement>) => {
+    if ((e.target as HTMLDivElement).id === "wrapper") {
+      onClose();
     }
-  }
+    
+  };
   return (
-    <div className="  fixed inset-0   bg-opacity-25 backdrop-blur-sm flex justify-center items-center " id="wrapper" onClick={handleClose}>
-      <div className="  mx-auto  bg-white/60 w-[400px]  flex flex-col rounded-lg">
-        <button
-          onClick={onClose}
-          className="  text-secondary bg-primary px-[10px] py-[3px] rounded-md text-lg  place-self-end font-bold "
+    <AnimatePresence>
+      {isvisible && (
+        <motion.div
+          variants={backdrop}
+          initial="hidden"
+          animate="visible"
+          className="  fixed top-0 left-0 w-screen h-screen inset-0 bg-black/25  bg-opacity-60 backdrop-blur-sm flex justify-center items-center "
+          id="wrapper"
+          onClick={handleClose}
         >
-          X
-        </button>
-        <div className="bg-white/60 p-2 ">{children}</div>
-      </div>
-    </div>
+          <motion.div
+            variants={modal}
+            className="  mx-auto shadow-sm bg-white w-[400px]  flex flex-col rounded-lg"
+          >
+            <button
+              onClick={onClose}
+              className="  text-secondary bg-primary px-[10px] py-[3px] rounded-md text-lg  place-self-end font-bold "
+            >
+              X
+            </button>
+            <div className=" bg-white shadow-sm p-2 ">{children}</div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
